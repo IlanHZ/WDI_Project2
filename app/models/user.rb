532c6de8ab593b5ff463123725ac450a9c90a61b
Ilan Hazan <ilaneliehazan@gmail.com>
2016-02-20 16:33:36 +0000

    passwordcolumn

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,8 @@
 class User < ActiveRecord::Base
+  include BCrypt
+  validates :email, presence: true, uniqueness: true
+  validates :username, presence: true, uniqueness: true
+  validates :password, presence: true
+  # will create two virtual attributes:
+  has_secure_password
 end
