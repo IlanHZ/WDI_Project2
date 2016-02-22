ffce959bdf44f55e9bea5cf4975044f1ce1ca000
Ilan Hazan <ilaneliehazan@gmail.com>
2016-02-22 10:01:43 +0000

    new recipient and sender id column

--- db/schema.rb
+++ db/schema.rb
@@ -11,15 +11,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160222095121) do
+ActiveRecord::Schema.define(version: 20160222100042) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "messages", force: :cascade do |t|
-    t.string "message_content"
-    t.string "recipient_id"
-    t.string "title"
+    t.string  "message_content"
+    t.string  "title"
+    t.integer "sender_id"
+    t.integer "recipient_id"
   end
 
   create_table "properties", force: :cascade do |t|
