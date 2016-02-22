11b5f10e11ddb3c30adfe96bc1d21483d4121c07
Ilan Hazan <ilaneliehazan@gmail.com>
2016-02-22 19:52:46 +0000

    images upload but don't display

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160222124526) do
+ActiveRecord::Schema.define(version: 20160222193721) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -45,6 +45,7 @@ ActiveRecord::Schema.define(version: 20160222124526) do
     t.string  "password_digest"
     t.string  "comment"
     t.integer "property_id"
+    t.string  "profile_pic"
   end
 
 end
