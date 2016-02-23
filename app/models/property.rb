1e4b207dffec4af9137c4e8563063ab60c0fb633
Ilan Hazan <ilaneliehazan@gmail.com>
2016-02-23 13:33:15 +0000

    before reseting database

--- app/models/property.rb
+++ app/models/property.rb
@@ -1,3 +1,10 @@
+require 'carrierwave/orm/activerecord'
+require_relative '../uploaders/profile_pic_uploader'
+
+
 class Property < ActiveRecord::Base
   belongs_to :user
+
+
+  mount_uploader :profile_pic, ProfilePicUploader
 end
