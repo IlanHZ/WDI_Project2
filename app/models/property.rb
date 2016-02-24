5971e811ca5610ec36e7737c35e84ef659ac6ea9
Ilan Hazan <ilaneliehazan@gmail.com>
2016-02-24 09:56:07 +0000

    presence validation for gmaps

--- app/models/property.rb
+++ app/models/property.rb
@@ -3,8 +3,12 @@ require_relative '../uploaders/profile_pic_uploader'
 
 
 class Property < ActiveRecord::Base
+  validates :address1, presence: true
+  validates :postcode, presence: true
+  validates :lat, presence: true
+  validates :lng, presence: true
+  
   belongs_to :user
-
 
   mount_uploader :profile_pic, ProfilePicUploader
 end
