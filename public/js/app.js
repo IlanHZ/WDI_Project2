a4bf10fd973bb8495937c32b37c42b22f66056d7
Ilan Hazan <ilaneliehazan@gmail.com>
2016-02-24 15:23:41 +0000

    jQuery Up and Down

--- public/js/app.js
+++ public/js/app.js
@@ -83,5 +83,24 @@ $(function() {
 
   }
 
+      // Cache a reference to the hidden content.
+      var hiddenContent = $( "div.content" );
+      // Bind to the Read More link to toggle the
+      $( "a.readMore" ).click(
+          function( event ){
+              // Cancel the default event (this isn't a real link).
+              event.preventDefault();
+              // Check to see if the content is visible.
+              if (hiddenContent.is( ":visible" )){
+                  // Hide it slowly.
+                  hiddenContent.slideUp( 3000 );
+              } else {
+                  // Show it slowly.
+                  hiddenContent.slideDown( 6000 );
+              }
+          }
+      );
+
+
 
 });
