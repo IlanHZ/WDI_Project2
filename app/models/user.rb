38f43a423b9a142803e91593a90ae8f37d7b0f6d
Ilan Hazan <ilaneliehazan@gmail.com>
2016-02-24 13:55:02 +0000

    before removing property id

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,7 +10,7 @@ class User < ActiveRecord::Base
   validates :password, presence: true
   
   has_secure_password   
-  belongs_to :property
+  has_one :property
 
   has_many :sent_messages, class_name: "Message", foreign_key: "sender_id"
   has_many :received_messages, class_name: "Message", foreign_key: "recipient_id"
